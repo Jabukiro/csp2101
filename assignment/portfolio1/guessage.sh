0b25697fa2ae115fbf519d77ad42fb09b775664e
jabukiro <d.barihuta@gmail.com>
2020-03-31 00:07:50 +0800

    Add Non-Number filtering. Colour Output

--- assignment/portfolio1/guessage.sh
+++ assignment/portfolio1/guessage.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
-source ~/CSP2101/workshop/week5/colourSnippet.sh
-f=$((120*RANDOM/32767))
+#Implement --help
+#
+source ../../workshop/week5/colourSnippet.sh
+target=$((120*RANDOM/32767))
 echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
                                      Welcome to Guessing Game\n
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 "
-echo $f > secret.txt
+echo $target > secret.txt
 ./guessageBot.sh
+numberReg='^[0-9]$'
 while true
 do
 	read -p "Enter guess for age >" guess
+	if ! [[ $guess =~ ^[+-]?[0-9]+$ ]]; then
+		echo "Input not a number."
+		continue
+	fi
+
 	if (( guess < 0 )); then
 		echo "Bye Quiter!"
 		exit 0
 		break	
 	fi
-	if (( guess == f )); then
-		echo "Your guess is correct!!!! $f yo"
+	if (( guess == target )); then
+		echo -e "${GREEN}Your guess is correct!!!! $target yo${NC}"
 		exit 0
-	elif (( guess < f )); then
-		echo "Your guess is too low"
-	elif (( guess > f )); then
-		echo "Your guess is too high"
+	elif (( guess < target )); then
+		echo -e "${BLUE}Your guess is too low${NC}"
+	elif (( guess > target )); then
+		echo -e "${RED}Your guess is too high${NC}"
 	fi
 done
